495c65467d4b8f68feb4884021897adce8a0bc6b
Shivam Shukla <shuklashivam33221@gmail.com>
2025-03-10 12:08:13 +0530

    new add

---
+++ day8Q1.cpp
@@ -0,0 +1,14 @@
+// 283. Move Zeros
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int n=nums.size();
+            int zeroasnum=0;
+            for(int i=0;i<n;i++){
+                if(nums[i]!=0){
+                    swap(nums[i],nums[zeroasnum]);
+                    zeroasnum++;
+                }
+            }
+        }
+    };
